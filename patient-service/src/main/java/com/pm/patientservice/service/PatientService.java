d1d7bf730d26b15b03a767ca910c1f5e8558aa1b
Rasindu-Dulshan <rseekkubadu@gmail.com>
2025-08-20 10:12:06 +0530

    add custom exception to email validation in service layer

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientRequestDto;
 import com.pm.patientservice.dto.PatientResponseDto;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repositary.PatientRepositary;
@@ -23,6 +24,10 @@ public class PatientService {
     }
 
     public PatientResponseDto createPatient(PatientRequestDto patientRequestDto) {
+        if(patientRepositary.existsByEmail(patientRequestDto.getEmail())) {
+            throw new EmailAlreadyExistsException("Patient with email " + patientRequestDto.getEmail() + " already exists");
+        }
+
         Patient newPatient =  patientRepositary.save(PatientMapper.toModel(patientRequestDto));
         return PatientMapper.toDto(newPatient);
     }
