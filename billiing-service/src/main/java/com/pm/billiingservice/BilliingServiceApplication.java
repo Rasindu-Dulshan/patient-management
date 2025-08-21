41ba6dbb7fcfc57bbf300cb1d05768fe331841ba
Rasindu-Dulshan <rseekkubadu@gmail.com>
2025-08-21 10:43:57 +0530

    rename wrong billiing file

---
+++ billiing-service/src/main/java/com/pm/billiingservice/BilliingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.billiingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilliingServiceApplication.class, args);
+    }
+
+}
---
+++ billiing-service/src/test/java/com/pm/billiingservice/BilliingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.billiingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
